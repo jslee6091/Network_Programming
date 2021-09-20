7840e5dea547b5271c48ad9a93d2870e826552f5
jslee6091 <jslee6091@naver.com>
2021-09-20 16:57:06 +0900

    shared_memory

--- server.c
+++ server.c
@@ -6,18 +6,28 @@
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
 
 #define MAXLINE 1024
 #define PORTNUM 3500
+
+struct userinfo{
+	int usernumber;
+	int client_fd_list[10];
+	char userList[10][MAXLINE];
+};
 
 int main(int argc, char *argv[]) {
 	int listen_fd, client_fd;
 	pid_t pid;
 	socklen_t addrlen;
 	int readn, readname;
-	int usernumber = 0;
-	int client_fd_list[10];
-	char userList[10][MAXLINE];
+	struct userinfo *users;
+	void* shared_memory = (void*)0;
+	//int usernumber = 0;
+	//int client_fd_list[10];
+	//char userList[10][MAXLINE];
 	char userEnter[MAXLINE];
 	char buf[MAXLINE];
 	char exits[] = "exit\n";
@@ -69,8 +79,25 @@ int main(int argc, char *argv[]) {
 
 		printf("client accepted!, client_fd : %d\n", client_fd);
 
+		// make shared memory
+		int shmid = shmget((key_t)1234, sizeof(struct userinfo), 0666|IPC_CREAT);
+		if(shmid == -1){
+			perror("shared memory failed : ");
+			exit(1);
+		}
+
+		// attach to the shared memory
+		shared_memory = shmat(shmid, (void*)0, 0);
+		if(shared_memory == (void*)-1){
+			perror("shmat failed : ");
+			exit(1);
+		}
+
+		// wrte or read data to shared memory
+		users = (struct userinfo*)shared_memory;
+
 		// if user number get over the limit, send message to client
-		if(usernumber >= 10){
+		if(users->usernumber >= 10){
 			char* message = "Number of user is maximum";
 			int writeOver = write(client_fd, message, sizeof(message));
 			if(writeOver <= 0){
@@ -86,12 +113,16 @@ int main(int argc, char *argv[]) {
 		}
 
 		// register username and client_socket_number(client_fd)
-		strcpy(userList[usernumber], userEnter);
-		client_fd_list[usernumber]= client_fd;
-		printf("client name : %s\n", userList[usernumber]); 
-		printf("client fd : %d\n", client_fd_list[usernumber]); 
-		printf("user num : %d\n", usernumber);
-		usernumber++;
+		users->client_fd_list[users->usernumber] = client_fd;
+		strcpy(users->userList[users->usernumber], userEnter);
+		users->usernumber++;
+
+		//strcpy(userList[usernumber], userEnter);
+		//client_fd_list[usernumber]= client_fd;
+		printf("client name : %s\n", users->userList[users->usernumber-1]); 
+		printf("client fd : %d\n", users->client_fd_list[users->usernumber-1]); 
+		printf("user num : %d\n", users->usernumber);
+		//usernumber++;
 
 		pid = fork();
 
@@ -105,9 +136,9 @@ int main(int argc, char *argv[]) {
 					exit(1);
 				}
 
-				for(int i = 0; i < usernumber; i++){
-					printf("user client list : %s\n", userList[i]);
-					printf("client fd list : %d\n", client_fd_list[i]);
+				for(int i = 0; i < users->usernumber; i++){
+					printf("user client list : %s\n", users->userList[i]);
+					printf("client fd list : %d\n", users->client_fd_list[i]);
 				}
 
 				//char messages[MAXLINE] = strcat(userList[usernumber], " : ");
@@ -116,17 +147,17 @@ int main(int argc, char *argv[]) {
 				//printf(strcat(userList[usernumber], " : "));
 
 				// write message to other clients
-				for(int i = 0; i < usernumber; i++){
-					int writemsg = write(client_fd_list[i], buf, MAXLINE);
+				for(int i = 0; i < users->usernumber; i++){
+					int writemsg = write(users->client_fd_list[i], buf, MAXLINE);
 					if(writemsg <= 0){
 						perror("write wrong\n");
 						exit(1);
 					}
-					printf("%s : %s", userList[usernumber - 1], buf);
+					printf("%s : %s", users->userList[users->usernumber - 1], buf);
 				}
 
 				if(strcmp(buf, exits) == 0){
-					printf("%s is exit\n", userList[usernumber - 1]);
+					printf("%s is exit\n", users->userList[users->usernumber - 1]);
 					int writeExit = write(client_fd, exits, sizeof(exits));
 					if(writeExit <= 0){
 						perror("exit write error\n");
@@ -135,8 +166,15 @@ int main(int argc, char *argv[]) {
 				}
 			}
 			close(client_fd);
-			usernumber--;
+			users->usernumber--;
 			return 0;
+		}
+		if(shmctl(shmid, IPC_RMID, 0) == -1){
+			printf("shared memory remove failed : ");
+			return -1;
+		}
+		else{
+			printf("shared memory removed!\n");
 		}
 	}
 	close(listen_fd);
